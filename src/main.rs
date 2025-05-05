01310269e3a7aec2df84c168b5c30f71c34bc638
Bill P <bill@MoonUnit>
2025-05-05 16:03:05 -0500

    Application now outputs JSON.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router};
+use axum::routing::get;
+use axum::http::StatusCode;
+use serde::Serialize;
+
+// ------
+//  Main
+// ------
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root));
+        
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+// ------------------
+//  REQUEST HANDLERS
+// ------------------
+#[derive(Serialize)]
+struct Response {
+    message: &'static str,
+}
+
+async fn root() -> (StatusCode, Json<Response>) {
+    let response = Response {
+        message: "This app tracks fascists!",
+    };
+    
+    (StatusCode::OK, Json(response))
+}
+// async fn root() -> &'static str {
+//     "This app tracks fascists!"
+// }
