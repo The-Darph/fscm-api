e985eaa69f6da1c458deca759635cab946e3475b
Bill P <bill@MoonUnit>
2025-05-05 21:46:50 -0500

    Create database migrations.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Fascism Tracker API
+
+> A collaborative account of every significant fascist event enacted by the current U.S. administration until it either ends or it ends us...
+
+## Purpose
+
+The purpose of this API is to provide a backend to pull data related to these fascists acts. It serves as an archive
+and a way to search, sort, and filter these things so people can get an idea of the scale and speed of what is unfolding.
+
+## Contributing
+
+For simplicity and security, all contributions will happen via database
+seeds. You write a database seed using standard SQL statements.
+
+### What counts as a "fascist event"
+
+We're tracking two main types of events: Fascism and events leading to World War III or similar wars.
---
+++ migrations/2025-05-06-015454_create_subtypes/up.sql
@@ -0,0 +1,26 @@
+CREATE TABLE events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type INTEGER NOT NULL,
+    subtype INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    body TEXT NOT NULL,
+    scale INTEGER NOT NULL,
+    source TEXT NOT NULL,
+    transpired TEXT DEFAULT NULL,
+    published_date TEXT DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY(type) REFERENCES types(id),
+    FOREIGN KEY(subtype) REFERENCES subtypes(id)
+);
+
+-- CREATE TABLE events (
+--   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--   event_type INTEGER NOT NULL,
+--   FOREIGN KEY(event_type) REFERENCES type(id),
+--   subtype_id INTEGER NOT NULL,
+--   FOREIGN KEY(subtype_id) REFERENCES subtype(id),
+--   title VARCHAR NOT NULL,
+--   body TEXT NOT NULL,
+--   scale INTEGER NOT NULL,
+--   transpired TEXT DEFAULT NULL,
+--   published_date TEXT DEFAULT CURRENT_TIMESTAMP
+-- )
