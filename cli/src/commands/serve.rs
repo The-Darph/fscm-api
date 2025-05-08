d77d4b1a2adbdea2976b45ac1812e9f787fbb905
Mild Bill <billpatrianakos@users.noreply.github.com>
2025-05-08 14:44:00 -0500

    Add subcommands to CLI manager.
    CLI manager application being provided with ways of configuring the
    application.
    TODO: Create a way to start the server after loading settings.

--- cli/src/commands/serve.rs
+++ cli/src/commands/serve.rs
@@ -3,10 +3,19 @@ use clap::{ArgMatches, Command};
 pub const COMMAND_NAME: &str = "serve";
 
 pub fn configure() -> Command {
-    Command::new(COMMAND_NAME).about("Start HTTP server")
+    Command::new(COMMAND_NAME).about("Start HTTP server").arg(
+        Arg::new("port")
+            .short('p')
+            .long("port")
+            .value_name("PORT")
+            .help("TCP port to listen on")
+            .default_value("8080")
+            .value_parser(value_parser!(u16)),
+    )
 }
 
 pub fn handle(_matches: &ArgMatches) -> anyhow::Result<()> {
+    let port: u16 = *matches.get_one("port").unwrap_or(&8080);
  
     println!("This will start the server when implemented and take another argument to specify port number");
 
