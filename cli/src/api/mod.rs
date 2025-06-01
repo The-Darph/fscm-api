cb1647a92ec6ef9cbb60626865c4a4251d1620b3
Bill P <bill@MoonUnit>
2025-05-31 23:18:30 -0500

    Added middleware that will allow CORS protection and exceptions for testing purposes.

--- cli/src/api/mod.rs
+++ cli/src/api/mod.rs
@@ -1,9 +1,20 @@
-use axum::Router;
+use axum::{Router, routing::get};
 mod handlers;
 mod v1;
 use crate::state::ApplicationState;
 use std::sync::Arc;
+use tower_http::cors::{CorsLayer, Any};
+use std::net::SocketAddr;
 
+// #[tokio::main]
 pub fn configure(state: Arc<ApplicationState>) -> Router {
-    Router::new().nest("/v1", v1::configure(state))
+    // Setup CORS middleware
+    let allowed_production = "https://your-production-site.com".parse().expect("Invalid origin URL");
+    let allowed_development = "http://localhost:9001".parse().expect("Invalid origin URL");
+    let cors = CorsLayer::new()
+        .allow_origin([allowed_production, allowed_development])
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    Router::new().nest("/v1", v1::configure(state)).layer(cors)
 }
